3c5622a6f6c20bb4ba7a7f7967b21ed8a743d7de
Junho Yeo <hanaro0704@gmail.com>
2021-03-02 03:14:56 +0900

    체크박스 컴포넌트에 체크 마크 추가

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 interface CheckboxProps
   extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
@@ -15,7 +15,9 @@ export const Checkbox: React.FC<CheckboxProps> = ({
 }) => {
   return (
     <Container onClick={() => onChange(!value)} {...props}>
-      <Square selected={value} />
+      <Square selected={value}>
+        <Checkmark />
+      </Square>
       <Text>{children}</Text>
     </Container>
   );
@@ -33,11 +35,36 @@ interface SquareProps {
 const Square = styled.div<SquareProps>`
   margin-top: 1px;
   margin-right: 8px;
-  width: 20px;
-  height: 20px;
+  width: 24px;
+  height: 24px;
   border-radius: 4px;
-  border: 1px solid #f4f4f4;
+  border: 3px solid #f4f4f4;
   background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${({ selected }) =>
+    selected &&
+    css`
+      border: 3px solid #ffe066;
+      background-color: #f59f00;
+    `};
+`;
+
+const Checkmark = styled.div`
+  display: inline-block;
+  margin-bottom: 3px;
+
+  &:after {
+    content: '';
+    display: block;
+    width: 4px;
+    height: 8px;
+    border: solid #fff;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+  }
 `;
 
 const Text = styled.span`
