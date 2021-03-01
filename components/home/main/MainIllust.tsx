5ad271820cb8abeb72217e373177950beae8743d
Junho Yeo <hanaro0704@gmail.com>
2021-03-01 21:52:40 +0900

    메인 섹션 모바일 지원

--- components/home/main/MainIllust.tsx
+++ components/home/main/MainIllust.tsx
@@ -18,32 +18,93 @@ export const MainIllust: React.FC = () => (
 );
 
 const IllustWrapper = styled.div`
+  margin-bottom: 36px;
   position: absolute;
   top: 0;
   right: 0;
+  height: 430px;
+
+  /* 85% */
+  @media (max-width: 950px) {
+    height: 366px;
+  }
+
+  @media (max-width: 850px) {
+    position: unset;
+  }
+
+  /* 80% */
+  @media (max-width: 600px) {
+    height: 344px;
+  }
+
+  /* 60% */
+  @media (max-width: 400px) {
+    height: 258px;
+  }
 `;
 
 const IllustContainer = styled.div`
   position: relative;
 `;
 
-const ChromeAppIcon = styled.img`
+const AppIcon = styled.img`
   width: 246px;
   height: 246px;
+  border-radius: 58px;
+
+  /* 85% */
+  @media (max-width: 950px) {
+    width: 210px;
+    height: 210px;
+    border-radius: 52px;
+  }
+
+  /* 80% */
+  @media (max-width: 600px) {
+    width: 198px;
+    height: 198px;
+    border-radius: 48px;
+  }
+
+  /* 60% */
+  @media (max-width: 400px) {
+    width: 148px;
+    height: 148px;
+    border-radius: 52px;
+    border-radius: 38px;
+  }
+`;
+
+const ChromeAppIcon = styled(AppIcon)`
   background-color: white;
   box-shadow: 16px 32px 67px -15px rgba(255, 0, 0, 0.14);
-  border-radius: 58px;
   position: absolute;
   top: 184px;
   right: 155px;
+
+  /* 85% */
+  @media (max-width: 950px) {
+    top: 156px;
+    right: 132px;
+  }
+
+  /* 80% */
+  @media (max-width: 600px) {
+    top: 147px;
+    right: 124px;
+  }
+
+  /* 60% */
+  @media (max-width: 400px) {
+    top: 110px;
+    right: 93px;
+  }
 `;
 
-const ClubhouseAppIcon = styled.img`
-  width: 246px;
-  height: 246px;
+const ClubhouseAppIcon = styled(AppIcon)`
   background-color: #adadad;
   box-shadow: -16px 32px 48px rgba(0, 0, 0, 0.25);
-  border-radius: 58px;
   position: absolute;
   top: 0;
   right: 0;
@@ -56,21 +117,83 @@ const DummyTextContainer = styled.div`
   position: absolute;
   top: 305px;
   right: 0;
+
+  /* 85% */
+  @media (max-width: 950px) {
+    top: 260px;
+  }
+
+  /* 80% */
+  @media (max-width: 600px) {
+    top: 244px;
+  }
+
+  /* 60% */
+  @media (max-width: 400px) {
+    top: 184px;
+  }
 `;
 
-const DummyTextOne = styled.div`
-  width: 276px;
+const DummyTextBase = styled.div`
   height: 42px;
   background: #ffffff;
   box-shadow: -8px 8px 36px rgba(0, 0, 0, 0.14);
   border-radius: 32px;
+
+  /* 85% */
+  @media (max-width: 950px) {
+    height: 36px;
+  }
+
+  /* 80% */
+  @media (max-width: 600px) {
+    height: 34px;
+  }
+
+  /* 60% */
+  @media (max-width: 400px) {
+    height: 24px;
+  }
 `;
 
-const DummyTextTwo = styled.div`
+const DummyTextOne = styled(DummyTextBase)`
+  width: 276px;
+
+  /* 85% */
+  @media (max-width: 950px) {
+    width: 236px;
+  }
+
+  /* 80% */
+  @media (max-width: 600px) {
+    width: 220px;
+  }
+
+  /* 60% */
+  @media (max-width: 400px) {
+    width: 166px;
+  }
+`;
+
+const DummyTextTwo = styled(DummyTextBase)`
   margin-top: 12px;
   width: 178px;
-  height: 42px;
-  background: #ffffff;
-  box-shadow: -8px 8px 36px rgba(0, 0, 0, 0.14);
-  border-radius: 32px;
+
+  /* 85% */
+  @media (max-width: 950px) {
+    margin-top: 10px;
+    width: 152px;
+  }
+
+  /* 80% */
+  @media (max-width: 600px) {
+    margin-top: 9px;
+    width: 142px;
+  }
+
+  /* 60% */
+  @media (max-width: 400px) {
+    margin-top: 7px;
+    width: 108px;
+  }
 `;
--- components/home/main/MainSection.tsx
+++ components/home/main/MainSection.tsx
@@ -5,42 +5,61 @@ import { MainIllust } from './MainIllust';
 
 export const MainSection: React.FC = ({ children }) => {
   return (
-    <Main>
-      <MainIllust />
-      <Content>
-        <Title>
-          클럽하우스 프로필을
-          <br />
-          <span>링크 하나로</span>
-          <br />
-          웹으로 공유하세요 🚀
-        </Title>
-        <Description>
-          닉네임을 외치면서 스스로를 홍보하는 것보다,
-          <br />
-          <strong>
-            프로필 링크를 츤츤하게 건네면 팔로우당할 확률이 높다는 것,
-          </strong>
-          <br />
-          알고 계셨나요!
-        </Description>
-        <Description>
-          사실 여러분의 프로필은 이미 여기 있어요.
-          <br />
-          사용자 이름만 입력하면 바로 볼 수 있죠!
-        </Description>
-        {children}
-      </Content>
-    </Main>
+    <Wrapper>
+      <Container>
+        <MainIllust />
+        <Content>
+          <Title>
+            클럽하우스 프로필을
+            <br />
+            <span>링크 하나로</span>
+            <br />
+            웹으로 공유하세요 🚀
+          </Title>
+          <Description>
+            닉네임을 외치면서 스스로를 홍보하는 것보다,
+            <br />
+            <strong>
+              프로필 링크를 츤츤하게 건네면{' '}
+              <span style={{ display: 'inline-block' }}>
+                팔로우당할 확률이 높다는 것,
+              </span>
+            </strong>
+            <br />
+            알고 계셨나요!
+          </Description>
+          <Description>
+            사실 여러분의 프로필은 이미 여기 있어요.
+            <br />
+            사용자 이름만 입력하면 바로 볼 수 있죠!
+          </Description>
+          {children}
+        </Content>
+      </Container>
+    </Wrapper>
   );
 };
 
-const Main = styled.div`
+const Wrapper = styled.div`
+  min-height: 100vh;
+`;
+
+const Container = styled.main`
   margin: 120px 0;
   width: 100%;
   position: relative;
   display: flex;
   flex-direction: column;
+
+  @media (max-width: 1100px) {
+    margin: 120px auto;
+    width: calc(100% - 60px);
+  }
+
+  @media (max-width: 850px) {
+    margin: 0px auto;
+    flex-direction: column-reverse;
+  }
 `;
 
 const Content = styled.div`
@@ -49,6 +68,13 @@ const Content = styled.div`
   left: 0;
   display: flex;
   flex-direction: column;
+  max-width: 474px;
+
+  @media (max-width: 850px) {
+    position: unset;
+    margin-top: 36px;
+    margin-bottom: 24px;
+  }
 `;
 
 const Title = styled.h1`
@@ -62,12 +88,27 @@ const Title = styled.h1`
   & > span {
     background-color: rgba(251, 205, 28, 0.65);
   }
+
+  @media (max-width: 1100px) {
+    font-size: 48px;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 36px;
+  }
 `;
 
 const Description = styled.p`
   margin: 0;
   margin-top: 12px;
-  max-width: 474px;
   font-size: 18.5px;
   line-height: 1.4;
+
+  @media (max-width: 1100px) {
+    font-size: 18px;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 16px;
+  }
 `;
