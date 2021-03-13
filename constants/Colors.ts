c7a8235335c17b7be630531f764a0228de6f05d1
Junho Yeo <hanaro0704@gmail.com>
2021-03-13 17:27:13 +0900

    클럽하우스 UI에서 사용되는 색상 팔레트 추가

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -10,3 +10,17 @@ export const ProductColors = {
   white: '#ffffff',
   placeholder: '#adb5bd',
 };
+
+export const ClubhouseColors = {
+  background_green: '#00a646',
+  button_green: '#57bf7b',
+  text_green: '#00a646',
+
+  background_red: '#e33f45',
+
+  background_ivory: '#f2efe4',
+  button_ivory: '#e7e3d5',
+  text_ivory: '#70664b',
+
+  text_black: '#262626',
+};
