f5e284cf8f33cc36b50af429b007d93aa77842ad
Junho Yeo <hanaro0704@gmail.com>
2021-03-13 17:11:16 +0900

    프로덕트에서 사용되는 색상 팔레트 추가

---
+++ constants/Colors.ts
@@ -0,0 +1,12 @@
+export const ProductColors = {
+  border_yellow: '#ffe066',
+  button_yellow: '#ffec99',
+  text_yellow: '#f59f00',
+  checkbox_yellow: '#f59f00',
+
+  border_gray: '#f4f4f4',
+  text_gray: '#575c61',
+
+  white: '#ffffff',
+  placeholder: '#adb5bd',
+};
