275f99bd59a63abd0182781acad74d0901e8bb03
Junho Yeo <hanaro0704@gmail.com>
2021-03-26 13:48:32 +0900

    랜딩 페이지의 프로모션 프로필 정보 추가

---
+++ constants/home/promotion.ts
@@ -0,0 +1,14 @@
+export const promotions = [
+  {
+    username: 'yeon.gyu.kim',
+    screenImage: require('@/assets/home/promotion/profile-1.png'),
+  },
+  {
+    username: 'junhoyeo',
+    screenImage: require('@/assets/home/promotion/profile-2.png'),
+  },
+  {
+    username: 'paul',
+    screenImage: require('@/assets/home/promotion/profile-3.png'),
+  },
+];
